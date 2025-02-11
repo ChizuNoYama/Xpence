a4b9fad4804f3156ab9692d2af9415de75c76401
Assane Dessables <titonton12144@hotmail.com>
2025-02-10 21:03:51 -0500

    wip: Binding ContextChanged and implemented ViewModel for WeeklyExpenseView

--- Xpence/Components/WeeklyExpenseView.xaml.cs
+++ Xpence/Components/WeeklyExpenseView.xaml.cs
@@ -7,9 +7,4 @@ public partial class WeeklyExpenseView
         InitializeComponent();
     }
     
-    public string? DisplayGroceryTotalAmount { get; set; }
-    public string? DisplayEatingOutTotalAmount { get; set; }
-    public string? DisplayShoppingTotalAmount { get; set; }
-    public string? DisplayTransportationTotalAmount { get; set; }
-    
 }
--- Xpence/ViewModels/Components/WeeklyExpenseViewModel.cs
+++ Xpence/ViewModels/Components/WeeklyExpenseViewModel.cs
@@ -2,5 +2,8 @@ namespace Xpence.ViewModels.Components;
 
 public class WeeklyExpenseViewModel(IServiceProvider serviceProvider) : BaseViewModel(serviceProvider)
 {
-    
+    public string? DisplayGroceryTotalAmount { get; set; }
+    public string? DisplayEatingOutTotalAmount { get; set; }
+    public string? DisplayShoppingTotalAmount { get; set; }
+    public string? DisplayTransportationTotalAmount { get; set; }
 }
